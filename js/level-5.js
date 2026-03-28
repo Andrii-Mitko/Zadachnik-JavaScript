b656a42e24628deaa034c04dbc6f681129b6ceb5
Andrii-Mitko <andreymit123@gmail.com>
2026-03-28 22:23:20 +0200

    level 5-7-1

--- js/level-5.js
+++ js/level-5.js
@@ -1,3 +1,8 @@
+
+//; "ЦЕЙ КОД  ДО ВСІХ ЗАВДАНЬ 5 РІВНЯ"
+
+
+
 const main = document.querySelector('main');
 main.style.cssText = 'display: flex; justify-content: center; flex-direction: column; align-items: center;';
 const title = document.querySelector('h1')
@@ -199,108 +204,452 @@ const title = document.querySelector('h1')
 
 //! =============================================
 
-// Уровень 5.3 задачника JavaScript
-// №1
+//; Уровень 5.3 задачника JavaScript
 
-// Дан абзац, инпут и кнопка. В инпут вводится число. По нажатию на кнопку выведите в абзац факториал введенного числа.
+//! =============================================
 
-// №2
+//? №1
+//? Дан абзац, инпут и кнопка. В инпут вводится число. По нажатию на кнопку выведите в абзац факториал введенного числа.
 
-// Даны два инпута, кнопка и абзац. В инпуты вводятся целые числа. Определите, какое из введенных чисел больше и выведите его в абзац.
+// title.textContent = 'Факторіал числа';
+// inputFirst.remove();
+// btn.addEventListener('click', e => {
+//     let fact = 1;
+//     const num = Number(inputSecond.value);
+//     if (isNaN(num) || num < 0) {
+//         pText.textContent = 'Некоректні дані!'
+//         return;
+//     } else {
+//     for (let i = 1; i <= num; i++) {
+//         fact *= i;
+//     }
+//     pText.textContent = fact;
+//         inputSecond.value = '';
+//         }
+// });
 
-// №3
+//! =============================================
 
-// Дана кнопка и инпут. В инпут вводится число. По нажатию на кнопку найдите сумму цифр введеного числа.
+//? №2
+//? Даны два инпута, кнопка и абзац. В инпуты вводятся целые числа. Определите, какое из введенных чисел больше и выведите его в абзац.
 
-// №4
+// title.textContent = 'Яке число більше?';
+// btn.addEventListener('click', () => {
+//   const num1 = Number(inputFirst.value);
+// const num2 = Number(inputSecond.value);
+// if (!Number.isInteger(num1) ||!Number.isInteger(num2)) {
+//     pText.textContent = 'Некоректні дані!';
+// } else {
+//     pText.textContent = Math.max(num1, num2);
+//     }
+//     inputFirst.value = '';
+//     inputSecond.value = '';
+// });
 
-// Дан абзац, инпут и кнопка. В инпут вводится слово. По нажатию на кнопку переверните слово и запишите его в абзац.
+//! =============================================
 
-// №5
+//? №3
+//? Дана кнопка и инпут. В инпут вводится число. По нажатию на кнопку найдите сумму цифр введеного числа.
 
-// Даны пустые абзацы и кнопка. По нажатию на кнопку заполните абзацы числами по порядку.
+// title.textContent = 'Сума цифр числа';
+// inputFirst.remove();
+// btn.addEventListener('click', () => {
+//     const numStr = inputSecond.value;
+//     if (isNaN(Number(numStr))) {
+//         pText.textContent = 'Некоректні дані!';
+//     } else {
+//         const sum = Math.abs(Number(numStr));
+//          pText.textContent = String(sum).split('').reduce((acc, n) => acc + Number(n), 0);
+//         }
+//     inputSecond.value = '';
+// });
 
-// №6
+//! =============================================
 
-// Дан инпут, абзац и кнопка.В инпут вводится число.По клику на кнопку выведите в абзац факториал этого числа.
+//? №4
+//? Дан абзац, инпут и кнопка. В инпут вводится слово. По нажатию на кнопку переверните слово и запишите его в абзац.
+
+// title.textContent = 'Зміна порядку слів';
+// inputFirst.remove();
+//         btn.addEventListener('click', () => {
+//             pText.textContent = inputSecond.value.split('').reverse().join('');
+//             inputSecond.value = '';
+//     });
+
+//! =============================================
+
+//? №5
+//? Даны пустые абзацы и кнопка. По нажатию на кнопку заполните абзацы числами по порядку.
+
+// title.textContent = 'Числа по порядку';
+// inputFirst.remove();
+// inputSecond.remove();
+// const pText2 = document.createElement('p');
+// pText2.style.cssText = 'margin: 15px; padding: 10px;';
+// div.append(pText2);
+// let num = null;
+// btn.addEventListener('click', e => {
+//         num += 1
+//         pText.textContent = num;
+//     pText2.textContent = num + 1;
+// });
+
+//! =============================================
+
+//? №6
+//? Дан инпут, абзац и кнопка.В инпут вводится число.По клику на кнопку выведите в абзац факториал этого числа.
+
+// title.textContent = 'Факторіал числа';
+// inputFirst.remove();
+// btn.addEventListener('click', () => {
+//     let fact = 1;
+//     const num = Number(inputSecond.value);
+//     if (isNaN(num) || num < 0) {
+//         pText.textContent = 'Некоректні дані!'
+//         return;
+//     } else {
+//     for (let i = 1; i <= num; i++) {
+//         fact *= i;
+//     }
+//     pText.textContent = fact;
+//         inputSecond.value = '';
+//         }
+// });
+
+//! =============================================
+
+//; Уровень 5.4 задачника JavaScript
+
+//! =============================================
+
+//? №1
+//? Даны абзацы с текстом. Сделайте так, чтобы по нажатию на любой абзац его текстом становилось значение '!'.
+
+// title.textContent = "Значення '!'";
+// inputFirst.remove();
+// inputSecond.remove();
+// btn.remove();
+// pText.textContent = 'Абзац с текстом';
+// const pText2 = document.createElement('p');
+// pText2.style.cssText = 'margin: 15px; padding: 10px;';
+// div.append(pText2);
+// pText2.textContent = 'Абзац с текстом';
+//     div.addEventListener('click', e => {
+//         if (e.target.tagName === 'P') {
+//             e.target.textContent = '!';
+//         };
+//     });
+
+//! =============================================
+
+//? №2
+//? Даны ссылки и кнопка. По нажатию на кнопку получите массив атрибутов href ссылок.
+
+// title.textContent = 'Отримати атрибути';
+// inputFirst.remove();
+// inputSecond.remove();
+// const linkFirst = document.createElement('a');
+// const linkSecond = document.createElement('a');
+// div.append(linkFirst, linkSecond);
+// linkFirst.href = 'https://google.com';
+// linkFirst.textContent = 'Google';
+// linkSecond.href = 'https://chatgpt.com';
+// linkSecond.textContent = 'GPT';
+// btn.addEventListener('click', () => {
+//     const href1 = linkFirst.href;
+//     const href2 = linkSecond.href;
+//     const href = [href1, href2];
+//     pText.textContent = href;
+// });
+
+//! =============================================
+
+//? №3
+//? Дан абзац и кнопка. По нажатию на кнопку сделайте в цикле строку '123456789' и запишите ее в абзац.
+
+// title.textContent = 'У циклі рядок «123456789»';
+// inputFirst.remove();
+// inputSecond.remove();
+// btn.addEventListener('click', () => {
+//     let str = '';
+//         for (let i = 1; i < 10; i++) {
+//             str += String(i);
+//     }
+//     pText.textContent = str;
+// });
+
+
+//! =============================================
+
+//? №4
+//? Даны три абзаца. Выведите в первый текущий год, во второй - текущий месяц, в третий - текущий день.
+
+// title.textContent = 'Поточний рік, місяць, день';
+// inputFirst.remove();
+// inputSecond.remove();
+// const pText2 = document.createElement('p');
+// const pText3 = document.createElement('p');
+// pText2.style.cssText = 'margin: 15px; padding: 10px;';
+// pText3.style.cssText = 'margin: 15px; padding: 10px;';
+// div.append(pText2, pText3);
+//     btn.addEventListener('click', () => {
+//         const date = new Date();
+//         pText.textContent = date.getFullYear();
+//         pText2.textContent = date.toLocaleString('uk-UA', { month: 'long' });
+//         pText3.textContent = date.getDate();
+// });
+
+//! =============================================
+
+//? №5
+//? Дана ссылка, абзац и кнопка.По нажатию на кнопку запишите href ссылки в текст абзаца.
+
+// title.textContent = 'Отримати атрибути';
+// inputFirst.remove();
+// inputSecond.remove();
+// const linkFirst = document.createElement('a');
+// div.append(linkFirst);
+// linkFirst.href = 'https://google.com';
+// linkFirst.textContent = 'Google';
+// btn.addEventListener('click', () => {
+//     pText.textContent = linkFirst.href;
+// });
+
+//! =============================================
+
+//; Уровень 5.5 задачника JavaScript
+
+//! =============================================
+
+//? №1
+//? Дан инпут, кнопка и три абзаца. В инпут вводится дата в формате 2025-12-31. По нажатию на кнопку выведите в первый абзац год, во второй абзац - месяц, в третий день.
+
+// title.textContent = 'Рік, місяць, день';
+// inputFirst.remove();
+// const pText2 = document.createElement('p');
+// const pText3 = document.createElement('p');
+// pText2.style.cssText = 'margin: 15px; padding: 10px;';
+// pText3.style.cssText = 'margin: 15px; padding: 10px;';
+// div.append(pText2, pText3);
+//     btn.addEventListener('click', () => {
+//         const date = new Date(inputSecond.value);
+//         pText.textContent = date.getFullYear();
+//         pText2.textContent = date.toLocaleString('uk-UA', { month: 'long' });
+//         pText3.textContent = date.getDate();
+// });
+
+//! =============================================
+
+//? №2
+//? Даны два инпута и кнопка. В инпуты вводятся числа. По нажатию на кнопку заполните массив целыми числами от первого введеного числа до последнего.
+
+// title.textContent = 'масив чисел';
+//     btn.addEventListener('click', () => {
+//         const num1 = Number(inputFirst.value);
+//         const num2 = Number(inputSecond.value);
+//         if (isNaN(num1) || isNaN(num2)) {
+//             pText.textContent = 'Некоректні дані!';
+//         return;
+//         }
+//         let arr = [];
+//         for (let i = num1; i <= num2; i++) {
+//             arr.push(i);
+//         }
+//         pText.textContent = arr;
+// });
+
+//! =============================================
+
+//? №3
+//? Дан инпут, абзац и кнопка. В инпут вводится строка. По нажатию на кнопку выведите в абзац последний символ введенной строки.
+
+// title.textContent = 'Останній символ рядка';
+// inputFirst.remove();
+//     btn.addEventListener('click', () => {
+//         const str = inputSecond.value;
+//         pText.textContent = str[str.length - 1];
+// });
+
+//! =============================================
+
+//? №4
+//? Дан абзац, в котором записано число 1. По нажатию на кнопку увеличивайте число в абзаце на единицу.
+
+// title.textContent = 'Збільшення на одиницю';
+// inputFirst.remove();
+// inputSecond.remove();
+// pText.textContent = 1;
+// let num = Number(pText.textContent);
+// btn.addEventListener('click', () => {
+//     num += 1;
+//     pText.textContent = num;
+// });
+
+//! =============================================
+
+//? №5
+//? Дан инпут и абзац.В инпут вводится строка со словами.По потери фокуса выведите в абзац количество слов во введенной строке.
+
+// title.textContent = 'Кількість слів';
+// inputFirst.remove();
+// btn.remove();
+//     inputSecond.addEventListener('focusout', () => {
+//         const str = inputSecond.value.trim();
+//         pText.textContent = str.split(/\s+/).length;
+// });
+
+//! =============================================
+
+//; Уровень 5.6 задачника JavaScript
+
+//! =============================================
+
+//? №1
+//? Дан инпут, кнопка и абзац. В инпут вводится год. По нажатию на кнопку определите, введен високосный год или нет и выведите информацию об этом в абзац.
+
+// title.textContent = 'Високосний рік';
+// inputFirst.remove();
+//     btn.addEventListener('click', e => {
+//         const year = Number(inputSecond.value);
+//         if (isNaN(year)) {
+//             pText.textContent = 'Це не рік';
+//         } else {
+//             if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//                 pText.textContent = 'Це високосний рік';
+//             } else {
+//                 pText.textContent = 'Це звичайний рік';
+//             }
+//         }
+//         inputSecond.value = ''
+// });
+
+//! =============================================
+
+//? №2
+//? Дан инпут и кнопка. По нажатию на кнопку выведите в инпут случайное число из диапазона от 1 до 100.
+
+// title.textContent = 'Випадкове число';
+// inputFirst.remove();
+//     btn.addEventListener('click', () => {
+//         const randomNumber = Math.floor(Math.random() * 100) + 1;
+//         inputSecond.value = randomNumber;
+// });
+
+//! =============================================
+
+//? №3
+//? Дан инпут, кнопка и абзац. В инпут вводится дата в формате 2025-12-31. По нажатию на кнопку выведите в абзац день недели, соответствующий этой дате.
+
+// title.textContent = 'День недели';
+// inputFirst.remove();
+// const pText2 = document.createElement('p');
+// pText2.style.cssText = 'margin: 15px; padding: 10px;';
+// title.append(pText2);
+// pText2.textContent = 'Введіть дату';
+// const days = ['неділя', 'понеділок', 'вівторок', 'середа', 'четвер', 'пʼятниця', 'субота'];
+//     btn.addEventListener('click', e => {
+//         const date = new Date(inputSecond.value);
+//         if (isNaN(date.getTime())) {
+//             pText.textContent = 'Некоректна дата';
+//         } else {
+//             const day = date.getDay();
+//             pText.textContent = days[day];
+//         }
+//         inputSecond.value = '';
+// });
+
+//! =============================================
+
+//? №4
+//? Дан див с шириной, высотой и границей, и кнопка. По клику на кнопку увеличьте размеры дива в два раза.
+
+// title.textContent = 'Збільшення розміру div';
+// div.style.cssText += 'width: 200px; height: 200px; border: 2px solid red;';
+// btn.addEventListener('click', () => {
+//     // Получаем текущие значения из style
+//     let width = parseInt(div.style.width);   // "200px" → 200
+//     let height = parseInt(div.style.height); // "200px" → 200
+
+//     // Увеличиваем
+//     width *= 2;
+//     height *= 2;
+
+//     // Присваиваем обратно в style
+//     div.style.width = width + 'px';
+//     div.style.height = height + 'px';
+// });
+
+//! =============================================
+
+//? №5
+//? Даны ссылки.Сделайте так, чтобы по наведению на любую ссылку над ней появлялось значение ее атрибута href.
+
+// title.textContent = 'При наведенні курсору на будь-яке посилання над ним з’являлося значення його атрибута';
+// div.innerHTML = `
+//   <a href="https://google.com">Google</a>
+//   <a href="https://chat.openai.com" target="_blank">ChatGPT</a>
+//   <a href="https://uk.wikipedia.org" title="Вікіпедія українською">Вікіпедія</a>
+//   <a href="https://github.com" id="githubLink" class="external-link">GitHub</a>
+//   <a href="mailto:example@example.com">Написати лист</a>`;
+
+// const links = div.querySelectorAll('a');
+
+// links.forEach(link => {
+//   link.addEventListener('mouseenter', e => {
+//     // Создаём подсказку над ссылкой
+//     const tooltip = document.createElement('span');
+//     tooltip.textContent = link.getAttribute('href');
+//     tooltip.style.position = 'absolute';
+//     tooltip.style.background = 'yellow';
+//     tooltip.style.padding = '2px 5px';
+//     tooltip.style.border = '1px solid black';
+//     tooltip.style.fontSize = '12px';
+//     tooltip.classList.add('tooltip');
+
+//     // Добавляем в документ
+//     document.body.appendChild(tooltip);
+
+//     // Позиционируем над ссылкой
+//     const rect = link.getBoundingClientRect();
+//     tooltip.style.left = rect.left + 'px';
+//     tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+//     // Удаляем подсказку при уходе мыши
+//     link.addEventListener('mouseleave', () => {
+//       tooltip.remove();
+//     }, { once: true });
+//   });
+// });
 
 
 
+//! =============================================
 
+//; Уровень 5.7 задачника JavaScript
 
-// Уровень 5.4 задачника JavaScript
-// №1
+//! =============================================
 
-// Даны абзацы с текстом. Сделайте так, чтобы по нажатию на любой абзац его текстом становилось значение '!'.
+//? №1
+//? Даны теги li с числами, инпут и кнопка. По нажатию на кнопку найдите сумму чисел и выведите их в инпут.
 
-// №2
+// title.textContent = 'Сума чисел';
+// inputFirst.remove();
+// const list = document.createElement('ul');
+// div.append(list);
+// list.innerHTML = `
+//     <li>1</li>
+//     <li>2</li>
+//     <li>3</li>
+//     <li>4</li>
+//     <li>5</li>`;
+//         btn.addEventListener('click', e => {
+//             const listAll = document.querySelectorAll('li');
+//             let sum = 0;
+//             listAll.forEach(n => sum += Number((n.textContent)));
+//             inputSecond.value = sum;
+//     });
 
-// Даны ссылки и кнопка. По нажатию на кнопку получите массив атрибутов href ссылок.
-
-// №3
-
-// Дан абзац и кнопка. По нажатию на кнопку сделайте в цикле строку '123456789' и запишите ее в абзац.
-
-// №4
-
-// Даны три абзаца. Выведите в первый текущий год, во второй - текущий месяц, в третий - текущий день.
-
-// №5
-
-// Дана ссылка, абзац и кнопка.По нажатию на кнопку запишите href ссылки в текст абзаца.
-
-
-// Уровень 5.5 задачника JavaScript
-// №1
-
-// Дан инпут, кнопка и три абзаца. В инпут вводится дата в формате 2025-12-31. По нажатию на кнопку выведите в первый абзац год, во второй абзац - месяц, в третий день.
-
-// №2
-
-// Даны два инпута и кнопка. В инпуты вводятся числа. По нажатию на кнопку заполните массив целыми числами от первого введеного числа до последнего.
-
-// №3
-
-// Дан инпут, абзац и кнопка. В инпут вводится строка. По нажатию на кнопку выведите в абзац последний символ введенной строки.
-
-// №4
-
-// Дан абзац, в котором записано число 1. По нажатию на кнопку увеличивайте число в абзаце на единицу.
-
-// №5
-
-// Дан инпут и абзац.В инпут вводится строка со словами.По потери фокуса выведите в абзац количество слов во введенной строке.
-
-
-
-
-// Уровень 5.6 задачника JavaScript
-// №1
-
-// Дан инпут, кнопка и абзац. В инпут вводится год. По нажатию на кнопку определите, введен високосный год или нет и выведите информацию об этом в абзац.
-
-// №2
-
-// Дан инпут и кнопка. По нажатию на кнопку выведите в инпут случайное число из диапазона от 1 до 100.
-
-// №3
-
-// Дан инпут, кнопка и абзац. В инпут вводится дата в формате 2025-12-31. По нажатию на кнопку выведите в абзац день недели, соответствующий этой дате.
-
-// №4
-
-// Дан див с шириной, высотой и границей, и кнопка. По клику на кнопку увеличьте размеры дива в два раза.
-
-// №5
-
-// Даны ссылки.Сделайте так, чтобы по наведению на любую ссылку над ней появлялось значение ее атрибута href.
-
-
-
-// Уровень 5.7 задачника JavaScript
-// №1
-
-// Даны теги li с числами, инпут и кнопка. По нажатию на кнопку найдите сумму чисел и выведите их в инпут.
+//! =============================================
 
 // №2
 
